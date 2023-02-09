1f33150fc32267e7c0d054539f5bac2836d2970f
aripin <enterprisearipin@gmail.com>
2023-02-09 08:30:00 +0700

    fe

---
+++ sql/indocom.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: select * from kasir
+LIMIT 0, 1000
+
+-- Date: 2023-02-02 10:43
+*/
+INSERT INTO `` (`id_kasir`,`username`,`password`,`nama_kasir`) VALUES (1,'kasir1','kasir1','Kasir 1');
+INSERT INTO `` (`id_kasir`,`username`,`password`,`nama_kasir`) VALUES (2,'kasir2','kasir2','Kasir 2');
